30fbb117f0512395a651bae50ea481594ce8a5a8
Josivan Sousa <josivan.pereira@wconnect.com.br>
2024-01-04 12:09:37 -0300

    initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'prdmovida.blob.core.windows.net',
+            port: '',
+            pathname: '/public/imagens/**',
+          },
+        ],
+    },
+
+}
 
 module.exports = nextConfig
